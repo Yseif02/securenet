20f8f603438f9cb7aeb98a16eb25e640636fb605
yseif02 <aseif@mail.yu.edu>
2026-04-26 22:43:00 -0400

    done

--- ModernDataManagement/assignments/RedisLookup/src/main/java/edu/yu/mdm/RedisLookup.java
+++ ModernDataManagement/assignments/RedisLookup/src/main/java/edu/yu/mdm/RedisLookup.java
@@ -1,30 +1,42 @@
 package edu.yu.mdm;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 public class RedisLookup extends RedisLookupBase {
-    private static final String LOCATION_KEY_PREFIX = "location:";
-    private static final String BLOCK_KEY_PREFIX = "block:";
+    private static final String ACTIVE_NAMESPACE_KEY = "redislookup:active";
+    private static final String LOCATION_KEY_PREFIX = ":location:";
+    private static final String BLOCK_KEY_PREFIX = ":block:";
+    private static final String EXISTS_FIELD = "_exists";
+    private static final int PIPELINE_BATCH_SIZE = 10_000;
 
     public RedisLookup(Jedis conn, File blocks, File locations) {
         super(conn, blocks, locations);
-        conn.flushDB();
-        loadLocations(locations);
-        loadBlocks(blocks);
+        final String namespace = "redislookup:" + UUID.randomUUID();
+        loadLocations(namespace, locations);
+        loadBlocks(namespace, blocks);
+        conn.set(ACTIVE_NAMESPACE_KEY, namespace);
     }
 
     public RedisLookup(Jedis conn) {
         super(conn);
     }
 
-    private void loadLocations(final File locationsFile) {
+    private void loadLocations(final String namespace, final File locationsFile) {
         try (BufferedReader reader = new BufferedReader(new FileReader(locationsFile))) {
             String line = reader.readLine(); // skip header
+            Pipeline pipeline = conn.pipelined();
+            int pendingCommands = 0;
+
             while ((line = reader.readLine()) != null) {
                 if (line.isBlank()) {
                     continue;
@@ -44,32 +56,47 @@ public class RedisLookup extends RedisLookupBase {
                 final String countryName = safeValue(fields, 5);
                 final String timeZone = safeValue(fields, 12);
 
-                final String redisKey = LOCATION_KEY_PREFIX + locationId;
+                final Map<String, String> locationFields = new HashMap<>(4);
+                locationFields.put(EXISTS_FIELD, "1");
                 if (countryName != null) {
-                    conn.hset(redisKey, "country", countryName);
+                    locationFields.put("country", countryName);
                 }
                 if (timeZone != null) {
-                    conn.hset(redisKey, "timezone", timeZone);
+                    locationFields.put("timezone", timeZone);
                 }
                 if (continentName != null) {
-                    conn.hset(redisKey, "continent", continentName);
+                    locationFields.put("continent", continentName);
+                }
+
+                pipeline.hset(locationKey(namespace, locationId), locationFields);
+                pendingCommands++;
+
+                if (pendingCommands >= PIPELINE_BATCH_SIZE) {
+                    pipeline.sync();
+                    pipeline = conn.pipelined();
+                    pendingCommands = 0;
                 }
             }
+
+            pipeline.sync();
         }
         catch (IOException e) {
             throw new IllegalStateException("Unable to load locations file: " + locationsFile, e);
         }
     }
 
-    private void loadBlocks(final File blocksFile) {
+    private void loadBlocks(final String namespace, final File blocksFile) {
         try (BufferedReader reader = new BufferedReader(new FileReader(blocksFile))) {
             String line = reader.readLine(); // skip header
+            Pipeline pipeline = conn.pipelined();
+            int pendingCommands = 0;
+
             while ((line = reader.readLine()) != null) {
                 if (line.isBlank()) {
                     continue;
                 }
 
-                final String[] fields = parseCsvLine(line);
+                final String[] fields = parseBlockLine(line);
                 if (fields.length < 2) {
                     continue;
                 }
@@ -86,9 +113,17 @@ public class RedisLookup extends RedisLookupBase {
                     continue;
                 }
 
-                final String blockKey = BLOCK_KEY_PREFIX + baseIp;
-                conn.hset(blockKey, "locationId", locationId);
+                pipeline.set(blockKey(namespace, baseIp), locationId);
+                pendingCommands++;
+
+                if (pendingCommands >= PIPELINE_BATCH_SIZE) {
+                    pipeline.sync();
+                    pipeline = conn.pipelined();
+                    pendingCommands = 0;
+                }
             }
+
+            pipeline.sync();
         }
         catch (IOException e) {
             throw new IllegalStateException("Unable to load blocks file: " + blocksFile, e);
@@ -97,6 +132,20 @@ public class RedisLookup extends RedisLookupBase {
 
     private String[] parseCsvLine(final String line) {
         return line.split(",(?=(?:[^\"]*\\\"[^\"]*\\\")*[^\"]*$)", -1);
+    }
+
+    private String[] parseBlockLine(final String line) {
+        final int firstComma = line.indexOf(',');
+        if (firstComma < 0) {
+            return new String[] {line};
+        }
+
+        final int secondComma = line.indexOf(',', firstComma + 1);
+        if (secondComma < 0) {
+            return new String[] {line.substring(0, firstComma), line.substring(firstComma + 1)};
+        }
+
+        return new String[] {line.substring(0, firstComma), line.substring(firstComma + 1, secondComma)};
     }
 
     private String safeValue(final String[] fields, final int index) {
@@ -123,6 +172,14 @@ public class RedisLookup extends RedisLookupBase {
             return network;
         }
         return network.substring(0, slashIndex);
+    }
+
+    private String locationKey(final String namespace, final String locationId) {
+        return namespace + LOCATION_KEY_PREFIX + locationId;
+    }
+
+    private String blockKey(final String namespace, final String ipAddress) {
+        return namespace + BLOCK_KEY_PREFIX + ipAddress;
     }
 
     private long ipToLong(final String ip) {
@@ -167,20 +224,24 @@ public class RedisLookup extends RedisLookupBase {
             return null;
         }
 
-        final String blockKey = BLOCK_KEY_PREFIX + ipAddress;
-        final String locationId = conn.hget(blockKey, "locationId");
+        final String namespace = conn.get(ACTIVE_NAMESPACE_KEY);
+        if (namespace == null) {
+            return null;
+        }
+
+        final String locationId = conn.get(blockKey(namespace, ipAddress));
         if (locationId == null) {
             return null;
         }
 
-        final String locationKey = LOCATION_KEY_PREFIX + locationId;
-        final String country = conn.hget(locationKey, "country");
-        final String timezone = conn.hget(locationKey, "timezone");
-        final String continent = conn.hget(locationKey, "continent");
-
-        if (country == null && timezone == null && continent == null) {
+        final List<String> location = conn.hmget(locationKey(namespace, locationId), EXISTS_FIELD, "country", "timezone", "continent");
+        if (location.get(0) == null) {
             return null;
         }
+
+        final String country = location.get(1);
+        final String timezone = location.get(2);
+        final String continent = location.get(3);
 
         return new String[] {country, timezone, continent};
     }
